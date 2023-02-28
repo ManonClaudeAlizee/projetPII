b2aa4c15b8664b635ced92299feea55d61baea58
ManonClaudeAlizee <101116480+ManonClaudeAlizee@users.noreply.github.com>
2023-02-28 14:44:08 +0100

    add event form

--- Controllers/EventController.cs
+++ Controllers/EventController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using projetPII.Data;
+using projetPII.Models;
 
 namespace projetPII.Controllers;
 
@@ -23,5 +24,30 @@ public class EventController : Controller
             .ToListAsync();
 
         return View(events);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: Event/Create
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("Id,Title,Date,Description,Lieu")] Event ev)
+    {
+        //validation rules
+        if (ModelState.IsValid)
+        {
+            // Create new event in DB
+            _context.Add(ev);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        //redisplay form if failure
+        return View(ev);
     }
 }
